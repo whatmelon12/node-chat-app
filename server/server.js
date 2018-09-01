e887c5f8fa1bf6ae0699a3fcf3fd532f2f35554f
Omar Jesus Mejia <omargotico@hotmail.com>
2018-09-01 12:03:07 -0500

    Change io variable definition

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const io = require('socket.io')(http);
 
 const port = process.env.port || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -8,6 +10,6 @@ var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+http.listen(port, () => {
     console.log(`Started on port ${port}`)
 });
